636c72f22d7464465060fbb024e3df08cbd8348c
y21 <artur.fiedler54@gmail.com>
2020-01-18 21:17:26 +0100

    add weightdata

--- structures/config.go
+++ structures/config.go
@@ -1,7 +1,15 @@
 package structures
 
+// WeightData stores weights
+type WeightData struct {
+	CPU  float32 `json:"cpu"`
+	Mem  float32 `json:"mem"`
+	Ping float32 `json:"ping"`
+}
+
 // Config is the structure for the config file
 type Config struct {
-	Port         int32 `json:"port"`
-	PingInterval int32 `json:"pingInterval"`
+	Port         int32      `json:"port"`
+	PingInterval int32      `json:"pingInterval"`
+	Weights      WeightData `json:"weights"`
 }
