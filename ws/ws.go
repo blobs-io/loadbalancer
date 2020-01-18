78ed3cc6d5b9b3ce28370c4e41987dc90361f8bf
y21 <artur.fiedler54@gmail.com>
2020-01-18 19:56:13 +0100

    type no longer needed

--- ws/ws.go
+++ ws/ws.go
@@ -5,14 +5,9 @@ const (
 	OpcodeHello = 10
 )
 
-// WSHelloMessage is sent when the client connects to the server
-type WSHelloMessage struct {
-	Interval int32 `json:"interval"`
-}
-
-// WSMessage represents a message that is sent by one of the nodes to post statistics
-type WSMessage struct {
-	Op   uint8       `json:"op"`
-	Type string      `json:"t"`
-	Data interface{} `json:"d"`
+// Message represents a message that is sent by one of the nodes to post statistics
+type Message struct {
+	Op   uint8  `json:"op"`
+	Type string `json:"t"`
+	Data string `json:"d"`
 }
