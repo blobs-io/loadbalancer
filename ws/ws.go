01236cedce3eff5662be40558b8031cab62f48b5
y21 <artur.fiedler54@gmail.com>
2020-01-18 20:25:25 +0100

    add UpdateData struct

--- ws/ws.go
+++ ws/ws.go
@@ -5,6 +5,13 @@ const (
 	OpcodeHello = 10
 )
 
+// UpdateData is the data that is sent every N ms by every node
+type UpdateData struct {
+	CPU         uint8  `json:"cpu"`
+	Mem         uint8  `json:"mem"`
+	AccessToken string `json:"accessToken"`
+}
+
 // Message represents a message that is sent by one of the nodes to post statistics
 type Message struct {
 	Op   uint8  `json:"op"`
